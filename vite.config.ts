d47f00ba4fba0edc5b24bc414c3dc340ac4522d8
luisdorsm-design <luisdorsm@gmail.com>
2025-12-24 08:06:19 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Esto asegura que process.env.API_KEY esté disponible en el navegador
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
   build: {
